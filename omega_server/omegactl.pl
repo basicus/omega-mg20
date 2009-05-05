11f7be39fd796f1e23f4ed96b12840d41842740d
Sergey Butenin <s.butenin@gmail.com>
2009-05-05 18:14:10 +0000

    Добавлен сброс буфера. Необходимо выполнять после каждой комманды!

--- omega_server/omegactl.pl
+++ omega_server/omegactl.pl
@@ -10,10 +10,11 @@ socket(TSOCK, PF_UNIX, SOCK_STREAM,0);
 connect(TSOCK, sockaddr_un("/tmp/omega.ctl")) or print("ERROR!");
 
 print TSOCK "Hello!";
+TSOCK->flush;
 print TSOCK "quit";
-
+TSOCK->flush;
 #while (defined(my $messg = <TSOCK>)) {
 #        print $messg;
 #	        print TSOCK "Hello server!";
-TSOCK->flush;
+
 #			}
